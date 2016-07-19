46314fb2f6b0b6d98ae133bd1c690b0a32e1b64e
Shivya B <sbalac17@my.centennialcollege.ca>
2016-07-19 11:53:05 -0400

    Assignment5

---
+++ Assignment5/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+/*
+    Shivya Balachandran
+    Comp123
+    07/19/2016
+    300884989
+*/
+
+
+namespace Assignment5
+{
+    /**
+    * <summary>
+    * This class is the main program
+    * </summary>
+    * 
+    * @class Program
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice;
+            do
+            {
+                // print menu options
+                Console.WriteLine("1.Display Grades");
+                Console.WriteLine("2. Exit");
+                // prompt for choice
+                Console.Write("Choose something: ");
+                // accept choice
+                choice = Console.ReadLine();
+                // switch
+                switch (choice)
+                {
+                    case "1":
+                        Console.Write("enter file name: ");
+                        string fileName = Console.ReadLine();
+                        DisplayGrades(fileName);
+                        break;
+                    case "2":
+                        Console.WriteLine("goodbye");
+                        break;
+                    default: Console.WriteLine("Invalid option");
+                        break;
+                }
+
+            }
+            while (choice != "2");
+
+          
+        }
+        /**
+        * <summary>
+        * This method reads and displays students grades from a file
+        * </summary>
+        * 
+        * @static
+        * @method DisplayGrades
+        * @param {string} fileName
+        * @returns {void}
+        */
+        private static void DisplayGrades(string fileName)
+        {
+            try
+            { 
+                //reads the lines from the file 
+                using (StreamReader reader = new StreamReader(fileName))
+                {
+                    string line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        Student s = Student.Parse(line);
+                        Console.WriteLine(s);
+
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("No such file");
+            }
+        }
+    }
+}
---
+++ Assignment5/Student.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    /**
+    * <summary>
+    * This class contains students information
+    * </summary>
+    * 
+    * @class Student
+    */
+    class Student
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string ID { get; private set; }
+        public string Program { get; private set; }
+        public string Grade { get; private set; }
+
+        /**
+        * <summary>
+        * This constructor builds a student with grade information
+        * </summary>
+        * 
+        * @constructor
+        */
+        private Student(string firstName, string lastName, string id, string program, string grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            ID = id;
+            Program = program;
+            Grade = grade;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}, {1}: {2} {3}, {4} ", LastName, FirstName, ID, Program, Grade);
+        }
+
+        /**
+      * <summary>
+      * This utility method creates a student from a line of text 
+      * </summary>
+      * 
+      * @static
+      * @method Parse
+      * @param {string} line
+      * @returns {Student}
+      */
+        public static Student Parse(string line)
+        {// parses rows into students 
+            string[] columns = line.Split(':');
+            Student s = new Student(columns[0], columns[1], columns[2], columns[3], columns[4]);
+            return s; 
+        }
+    }
+
+}
